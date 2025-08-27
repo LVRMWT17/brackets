641a7a53048a537ec8fe2e12bf1f1b369f784e94
LVRMWT17 <lera.luckaya77@gmail.com>
2025-08-27 18:30:39 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const stack = [];
+  const bracketsMap = new Map();
+  bracketsConfig.forEach(([open, close]) => {
+    bracketsMap.set(open, close);
+  });
+  for (let i = 0; i < str.length; i += 1) {
+    const char = str[i];
+    if (bracketsMap.has(char)) {
+      if (char === bracketsMap.get(char)) {
+        if (stack.length > 0 && stack[stack.length - 1] === char) {
+          stack.pop();
+        } else {
+          stack.push(char);
+        }
+      } else {
+        stack.push(char);
+      }
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      const lastOpen = stack.pop();
+      const expectedClose = bracketsMap.get(lastOpen);
+
+      if (char !== expectedClose) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 };
